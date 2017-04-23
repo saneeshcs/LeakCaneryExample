36cf7f81a3c828f82e51e9f77b677c7c85681ac5
Chulliparambil Saneesh <saneeshcs@gmail.com>
2017-04-22 22:02:46 -0400

    Initial commit

---
+++ app/src/main/java/com/san/example/MyApplication.java
@@ -0,0 +1,31 @@
+package com.san.example;
+
+/**
+ * Created by Saneesh on 4/22/17.
+ */
+
+import android.app.Application;
+import android.os.StrictMode;
+import com.squareup.leakcanary.LeakCanary;
+
+public class MyApplication extends Application {
+
+    @Override public void onCreate() {
+        super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        enabledStrictMode();
+        LeakCanary.install(this);
+    }
+
+    private static void enabledStrictMode() {
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder() //
+                .detectAll() //
+                .penaltyLog() //
+                .penaltyDeath() //
+                .build());
+    }
+}
